105e2affbd04e21f8b3207b260be7f8bcd4e815a
mfedorych <>
2025-03-14 15:07:28 +0200

    add new

---
+++ tests/hw/coffeeCart.spec.ts
@@ -0,0 +1,46 @@
+import {test, expect} from '@playwright/test';
+import {consumers} from "node:stream";
+test('test', async ({ page }) => {
+
+        //#region
+        const goToBaseUrl = page.goto('https://coffee-cart.app/');
+        const userNameLocator = page.locator('#name')
+        const userName = `asdDas`
+        const emailFieldLocator = page.locator("#email")
+        const userEmail = `pokop@sad.com`
+        const espressoLocator = page.locator(`[data-test="Espresso"]`)
+        const espressoMacchiatoLocator = page.locator('[data-test="Espresso_Macchiato"]')
+        const cappucinoLocator = page.locator('[data-test="Cappucino"]')
+        const checkoutLocator = page.locator(`[data-test="checkout"]`)
+        const submitPaymentLocator = page.locator(`#submit-payment`)
+        const cartPageLocator = page.locator('//a[@href=\'/cart\']')
+        const yesDiscountButton = page.locator(`.yes`)
+
+        //#endregion
+        test('happyPath.test',  //просте замовленя кількох напоїв
+            async ({page}) => {
+                    await goToBaseUrl
+                    await espressoLocator.click();
+                    await espressoMacchiatoLocator.click();
+                    await checkoutLocator.click();
+                    await userNameLocator.fill(`${userName}`);
+                    await emailFieldLocator.fill(`${userEmail}`);
+                    await submitPaymentLocator.click();
+                    await expect(cartPageLocator).toContainText('cart (0)');
+            });
+
+        test('addAndCancelCart.test',
+            async ({page}) => {
+                await goToBaseUrl
+                await espressoLocator.click();
+                await espressoMacchiatoLocator.click();
+                await cappucinoLocator.click();
+                await yesDiscountButton.click();
+                await cartPageLocator.click();
+                await expect(checkoutLocator).toContainText('Total: $45.00');
+                await page.getByRole('button', { name: 'Remove one Espresso', exact: true }).click();
+                await page.getByRole('button', { name: 'Remove one Cappuccino' }).click();
+                await expect(checkoutLocator).toContainText('Total: $16.00');
+            });
+});
+
