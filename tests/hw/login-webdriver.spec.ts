415787473c68cf074ccb63f866a45e816437a550
mfedorych <>
2025-03-20 17:02:30 +0200

    test for login added. functions added. locators refactored

--- tests/hw/login-webdriver.spec.ts
+++ tests/hw/login-webdriver.spec.ts
@@ -1,36 +1,63 @@
-import {test, expect} from '@playwright/test';
+import {test, expect, Page} from '@playwright/test';
 
-test('login-webdriver.test', async ({page}) => {
-    const logOutButtonLocator = page.locator('.btn-outline-danger')
-    const signUpToolbarButtonLocator = page.locator('.btn-primary')
-    const signUpButtonLocator = page.locator('//*[contains(@class, \'btn btn-lg btn-primary pull-xs-right\')]')
+const username = "pok"
+const userEmail = `asd@dfs.co`
+const userPassword = `s213fas`
+const baseUrl = `https://demo.learnwebdriverio.com/`
+// const logOutButtonLocator = page.locator('.btn-outline-danger')
+// const signUpToolbarButtonLocator = page.locator(`//*[contains(@class, 'btn')]`)
+//const signUpButtonLocator = page.locator('//*[contains(@class, \'btn btn-lg btn-primary pull-xs-right\')]')
 
-    await page.goto('https://demo.learnwebdriverio.com/');
+async function goToBaseUrlAndClearCookies(page) {
+    await page.goto(baseUrl,
+        {
+            timeout: 60_000,
+        }
+    )
     await page.context().clearCookies();
-    await page.locator('//a[@href=\'/register\']').click();
-    await page.locator('//input[contains(@placeholder, \'Username\')]').fill(`asdf`);
-    await page.locator('//input[contains(@placeholder, \'Email\')]').fill('asdf@go.co');
-    await page.locator('//input[contains(@placeholder, \'Password\')]').fill('12345');
-    await signUpToolbarButtonLocator.click(); //button
-    await page.locator(`//a[contains(@href, 'settings')]`).click()
-    await expect(page.locator(`//*[@class='btn btn-outline-danger']`)).toBeVisible()
+}
+
+async function registerNewUser(
+    page: Page
+) {
+    await page.locator(`//*[@href='/register']`).click()
+    await page.locator(`//*[contains(@placeholder, 'sername')]`).fill(`${username}`)
+    await page.locator(`//*[contains(@placeholder, 'mail')]`).fill(`${userEmail}`)
+    await page.locator(`//*[contains(@placeholder, 'assword')]`).fill(`${userPassword}`)
+    await page.locator(`//*[contains(@class, 'btn')]`).click()
+}
+
+test('successful user registration ', async ({page}) => {
+    await goToBaseUrlAndClearCookies(page)
+    await registerNewUser(page)
+
+    await expect(page.locator(`//*[@class and contains(text(),'Home')]`)).toBeVisible()
 })
 
-test(`checkErrors`, async ({page}) => {
+test(`check errors during registration`, async ({page}) => {
     const signUpToolbarButtonLocator = page.locator('//a[@href=\'/register\']')
-    await page.goto('https://demo.learnwebdriverio.com/');
     const signUpButtonLocator = page.locator('//*[contains(@class, \'btn btn-lg btn-primary pull-xs-right\')]')
 
-    //blank fields block
+    await goToBaseUrlAndClearCookies(page)
+
+    //blank fields errors
     await signUpToolbarButtonLocator.click()
     await signUpButtonLocator.click()
+
     await expect(page.locator(`//*[contains(@class, 'error-messages')]//*[contains(text(),"username")]`)).toContainText("username can't be blank")
     await expect(page.locator(`//*[contains(@class, 'error-messages')]//*[contains(text(), "email")]`)).toBeVisible()
 
-    //exist block
+    //username and email already taken errors
     await page.locator('//input[contains(@placeholder, \'Username\')]').fill(`asdf`);
     await page.locator('//input[contains(@placeholder, \'Email\')]').fill('asdf@go.co');
     await signUpButtonLocator.click()
+
     await expect(page.locator(`//*[contains(@class, 'error-messages')]//*[contains(text(), "username is already taken")]`)).toBeVisible()
     await expect(page.locator(`//*[contains(@class, 'error-messages')]//*[contains(text(), "email is already taken")]`)).toBeVisible()
+
+    //invalid email format error
+    await page.locator('//input[contains(@placeholder, \'Email\')]').fill('21421');
+    await signUpButtonLocator.click()
+
+    await expect(page.locator(`//*[contains(@class, 'error-messages')]//*[contains(text(), "email is invalid")]`)).toBeVisible()
 })
